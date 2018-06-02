203f9ed522f5cbe9b8e8535d1dba84cd85761dcc
Kevin Scroggins <nitro404@gmail.com>
2018-06-02 16:57:56 -0400

    Updated gulp file to use gulp fabricator module

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,42 @@
-const gulp = require("gulp");
-const util = require("gulp-util");
-const rename = require("gulp-rename");
-const filter = require("gulp-filter");
-const sourcemaps = require("gulp-sourcemaps");
-const uglifyjs = require("uglify-js");
-const composer = require("gulp-uglify/composer");
-const pump = require("pump");
-const glob = require("glob");
-const changeCase = require("change-case");
-const browserify = require("browserify");
-const source = require("vinyl-source-stream");
-const buffer = require("vinyl-buffer");
+var gulp = require("gulp");
+var util = require("gulp-util");
+var rename = require("gulp-rename");
+var filter = require("gulp-filter");
+var sourcemaps = require("gulp-sourcemaps");
+var uglifyjs = require("uglify-js");
+var composer = require("gulp-uglify/composer");
+var pump = require("pump");
+var glob = require("glob");
+var changeCase = require("change-case");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+var buffer = require("vinyl-buffer");
+var fabricator = require("gulp-fabricator");
 
-var config = {
+fabricator.setup({
 	name: "Change Case",
-	paths: {
-		js: {
-			source: ["src/**/*.js"],
-			destination: "dist/",
-		}
+	base: {
+		directory: __dirname
 	}
-};
+});
 
 gulp.task("build:js", function(callback) {
 	pump([
 		browserify({
-			entries: glob.sync(config.paths.js.source[0]),
-			standalone: changeCase.camel(config.name),
+			entries: glob.sync(fabricator.config.js.source[0]),
+			standalone: changeCase.camel(fabricator.config.name),
 			debug: true
 		})
 		.bundle()
 		.on("error", util.log.bind(util, "Browserify Error"))
-		.pipe(source(changeCase.param(config.name) + "-bundled.js").on("error", util.log))
+		.pipe(source(changeCase.param(fabricator.config.name) + "-bundled.js").on("error", util.log))
 		.pipe(buffer())
 		.pipe(sourcemaps.init({ loadMaps: true }))
 		.pipe(sourcemaps.write("./"))
-		.pipe(gulp.dest(config.paths.js.destination).on("error", util.log))
-		.pipe(filter([config.paths.js.destination + "*.js", "!*.min.js", "!*.map"]))
+		.pipe(gulp.dest(fabricator.config.js.destination).on("error", util.log))
+		.pipe(filter([fabricator.config.js.destination + "*.js", "!*.min.js", "!*.map"]))
 		.pipe(rename({ extname: ".min.js" }).on("error", util.log)),
 		composer(uglifyjs, console)({ mangle: false }).on("error", util.log),
-		gulp.dest(config.paths.js.destination).on("error", util.log)
+		gulp.dest(fabricator.config.js.destination).on("error", util.log)
 	], callback);
 });
-
-gulp.task("build", ["build:js"]);
